c0f7117db7938872fb71c9eed5e92d64cfdcb82a
Filipe Santos <filipesantos@live.ca>
2023-09-05 21:43:04 -0400

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,17 @@
+import { type AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { type AccountModel } from '../../../../domain/models'
+import { type AddAccountModel } from '../../../../domain/usecases'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (account: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(account)
+    return await new Promise(resolve => {
+      resolve({
+        id: result.insertedId.toString(),
+        ...account
+      })
+    })
+  }
+}
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb'
+import { type Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient | null,
@@ -9,5 +9,8 @@ export const MongoHelper = {
     if (this.client) {
       await this.client.close()
     }
+  },
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
