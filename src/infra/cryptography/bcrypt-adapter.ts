6d778e8e487d9ff38c3e4f966f2cff73f131c1c2
fc-santos <filipesantos@live.ca>
2024-01-02 21:37:42 -0500

    feat: ensure BcryptAdapter returns false when compare fails

--- src/infra/cryptography/bcrypt-adapter.ts
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -6,8 +6,7 @@ export class BcryptAdapter implements Hasher, HashComparer {
   constructor (private readonly salt: number) { }
 
   async compare (value: string, hash: string): Promise<boolean> {
-    await bcrypt.compare(value, hash)
-    return await new Promise(resolve => { resolve(true) })
+    return await bcrypt.compare(value, hash)
   }
 
   async hash (value: string): Promise<string> {
