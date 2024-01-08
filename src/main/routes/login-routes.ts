be2e26c9c931519aa465cbba16720ff385611deb
fc-santos <filipesantos@live.ca>
2024-01-07 21:19:17 -0500

    feat: add login route

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,8 +1,11 @@
 import { type Router } from 'express'
-import { makeSignupController } from '../factories/signup/signup-factory'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
+import { makeSignupController } from '../factories/signup/signup-factory'
+import { makeLoginController } from '../factories/login/login-factory'
 
 export default (router: Router): void => {
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   router.post('/signup', adaptRoute(makeSignupController()))
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  router.post('/login', adaptRoute(makeLoginController()))
 }
