82319127f39631c81e3261acf80c1f53efb1e2ee
fc-santos <filipesantos@live.ca>
2024-01-07 21:19:33 -0500

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,10 +1,14 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { type Collection } from 'mongodb'
+import { hash } from 'bcrypt'
 
 const mongoURL = process.env.MONGO_URL
 
 if (!mongoURL) throw new Error('MONGO_URL is not defined')
+
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -12,7 +16,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -29,6 +33,24 @@ describe('Login Routes', () => {
           email: 'filipe.santos@gmail.com',
           password: '123',
           passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
+        name: 'Filipe',
+        email: 'filipe.santos@gmail.com',
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'filipe.santos@gmail.com',
+          password: '123'
         })
         .expect(200)
     })
